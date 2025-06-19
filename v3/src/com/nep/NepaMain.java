79623b29efdef52da70acd3f363feb0c80d7c864
liuguanghui520 <3043963076@qq.com>
2025-06-19 16:35:21 +0800

    退出登录功能√

---
+++ v3/src/com/nep/NepaMain.java
@@ -0,0 +1,72 @@
+package com.nep;
+
+import com.nep.controller.NepaMainViewController;
+import com.nep.controller.NepgLoginViewController;
+import com.nep.controller.NepmLoginViewController;
+import com.nep.controller.NepsLoginViewController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NepaMain extends Application {
+
+    public static Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        // 加载主选择界面并设置控制器的主应用引用
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/NepaMainView.fxml"));
+        Parent root = loader.load();
+        NepaMainViewController controller = loader.getController();
+        controller.setMainApp(this);
+
+        primaryStage.setTitle("东软环保监督平台 - 身份选择");
+        primaryStage.setScene(new Scene(root, 410, 413));
+        primaryStage.show();
+    }
+
+    // 显示主选择界面
+    public void showMainSelectView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/NepaMainView.fxml"));
+        Parent root = loader.load();
+        // 获取控制器实例并设置 mainApp
+        NepaMainViewController controller = loader.getController();
+        controller.setMainApp(this);
+        primaryStage.setTitle("东软环保监督平台 - 身份选择");
+        primaryStage.setScene(new Scene(root, 500, 350));
+        primaryStage.show();
+    }
+
+    // 显示管理端登录界面
+    public void showManagerLogin() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/NepmLoginView.fxml"));
+        primaryStage.setTitle("东软环保公众监督平台-管理端");
+        primaryStage.setScene(new Scene(root));
+        NepmLoginViewController.primaryStage=primaryStage;
+    }
+
+    // 显示网格员端登录界面
+    public void showGridLogin() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/NepgLoginView.fxml"));
+        primaryStage.setTitle("东软环保公众监督平台-网格员端");
+        primaryStage.setScene(new Scene(root));
+        NepgLoginViewController.primaryStage=primaryStage;
+    }
+
+    // 显示公众监督员端登录界面
+    public void showSupervisorLogin() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/NepsLoginView.fxml"));
+        primaryStage.setTitle("东软环保公众监督平台-公众监督员端");
+        primaryStage.setScene(new Scene(root));
+        NepsLoginViewController.primaryStage=primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ v3/src/com/nep/controller/NepsLoginViewController.java
@@ -0,0 +1,62 @@
+package com.nep.controller;
+
+import com.nep.NepsMain;
+import com.nep.entity.Supervisor;
+import com.nep.service.SupervisorService;
+import com.nep.service.impl.SupervisorServiceImpl;
+import com.nep.util.JavafxUtil;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NepsLoginViewController {
+
+    @FXML
+    private TextField txt_id;    //绑定登录账号文本框
+    @FXML
+    private PasswordField txt_password;    //绑定登录密码框
+
+    public TextField getTxt_id() {
+        return txt_id;
+    }
+
+    public void setTxt_id(TextField txt_id) {
+        this.txt_id = txt_id;
+    }
+
+    public PasswordField getTxt_password() {
+        return txt_password;
+    }
+
+    public void setTxt_password(PasswordField txt_password) {
+        this.txt_password = txt_password;
+    }
+
+    private SupervisorService service=new SupervisorServiceImpl();
+    public static Stage primaryStage;
+    public void login(){
+        Supervisor s=service.login(txt_id.getText(),txt_password.getText());
+        if(s!=null){
+            System.out.println("登录成功");
+            NepsSelectAqiViewController.primaryStage=primaryStage;
+            NepsSelectAqiViewController.supervisor=s;
+            JavafxUtil.showStage(NepsMain.class,"view/NepsSelectAqiView.fxml", primaryStage, "东软环保公众监督平台-公众监督员端-AQI数据反馈");
+        }else{
+            JavafxUtil.showAlert(primaryStage,"登陆失败","用户名或密码错误","a=error","warn");
+        }
+    }
+
+    public void register(){
+        System.out.println("注册功能");
+        NepsRegisterViewController.primaryStage=primaryStage;
+        JavafxUtil.showStage(NepsMain.class,"view/NepsRegisterView.fxml",primaryStage,"东软环保公众监督员平台——注册功能");
+    }
+
+
+}
